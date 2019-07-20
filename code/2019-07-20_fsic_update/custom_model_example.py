91716f8a9c82c272e953f90be396e988cd7fb6a7
Chris Thoung <chris.thoung@gmail.com>
2019-07-21 00:35:16 +0100

    EG: Add initial example script to then edit by hand

---
+++ code/2019-07-20_fsic_update/custom_model_example.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+custom_model_example
+====================
+Example of a hand-edited model definition based on a (further) simplified
+version of Godley and Lavoie's (2007) Model *SIM*.
+
+The code to generate the original model template is as follows:
+
+    import fsic
+
+    script = '''\
+C = {alpha_1} * YD + {alpha_2} * H[-1]
+YD = Y - T
+Y = C + G
+T = {theta} * Y
+H = H[-1] + YD - C
+'''
+
+    symbols = fsic.parse_model(script)
+    definition = fsic.build_model_definition(symbols)
+
+Reference:
+
+    Godley, W., Lavoie, M. (2007)
+    *Monetary economics:  
+    An integrated approach to credit, money, income, production and wealth*,
+    Palgrave Macmillan
+
+-------------------------------------------------------------------------------
+MIT License
+
+Copyright (c) 2019 Chris Thoung
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from fsic import BaseModel
+
+
+class Model(BaseModel):
+    ENDOGENOUS = ['C', 'YD', 'H', 'Y', 'T']
+    EXOGENOUS = ['G']
+
+    PARAMETERS = ['alpha_1', 'alpha_2', 'theta']
+    ERRORS = []
+
+    NAMES = ENDOGENOUS + EXOGENOUS + PARAMETERS + ERRORS
+    CHECK = ENDOGENOUS
+
+    LAGS = 1
+    LEADS = 0
+
+    def _evaluate(self, t):
+        self._C[t] = self._alpha_1[t] * self._YD[t] + self._alpha_2[t] * self._H[t-1]
+        self._YD[t] = self._Y[t] - self._T[t]
+        self._H[t] = self._H[t-1] + self._YD[t] - self._C[t]
+        self._Y[t] = self._C[t] + self._G[t]
+        self._T[t] = self._theta[t] * self._Y[t]
